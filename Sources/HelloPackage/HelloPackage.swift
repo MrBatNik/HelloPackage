f2adaa57366438915ec1076a519c9b4b37e38b60
Nikita <nicandreo@gmail.com>
2021-12-19 16:31:36 +0300

    Init #2

--- Sources/HelloPackage/HelloPackage.swift
+++ Sources/HelloPackage/HelloPackage.swift
@@ -1,9 +1,7 @@
 public struct HelloPackage {
     private let text = "Hello, World!"
     
-    public init() {
-        
-    }
+    public init() {}
 
     public func sayHello() -> String {
         return text
